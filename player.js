187e6c28c585263e84eafe77c189fac0c4534de6
George Shanti <georgeshanti@gmail.com>
2020-05-14 00:54:54 +0530

    Chrome fix

--- player.js
+++ player.js
@@ -17,35 +17,42 @@ class Note{
         }
     }
 }
-var notes = {
-    "a": new Note("C4"),
-    "w": new Note("Db4"),
-    "s": new Note("D4"),
-    "e": new Note("Eb4"),
-    "d": new Note("E4"),
-    "f": new Note("F4"),
-    "t": new Note("Gb4"),
-    "g": new Note("G4"),
-    "y": new Note("Ab4"),
-    "h": new Note("A4"),
-    "u": new Note("Bb4"),
-    "j": new Note("B4"),
-    "k": new Note("C5")
-}
 
 //create a synth and connect it to the master output (your speakers)
 const synth = new Tone.Synth().toMaster();
 var playing = false;
+var started = false;
 //play a middle 'C' for the duration of an 8th note
-document.addEventListener('keydown', function (e) {
-    if(notes[e.key]!=undefined){
-        document.getElementById(e.key).classList.add("selected");
-        notes[e.key].play()
+
+document.querySelector('button').addEventListener('click', async () => {
+	await Tone.start();
+    console.log('audio is ready');
+    document.getElementById("disclaimer").style.display="none";
+    var notes = {
+        "a": new Note("C4"),
+        "w": new Note("Db4"),
+        "s": new Note("D4"),
+        "e": new Note("Eb4"),
+        "d": new Note("E4"),
+        "f": new Note("F4"),
+        "t": new Note("Gb4"),
+        "g": new Note("G4"),
+        "y": new Note("Ab4"),
+        "h": new Note("A4"),
+        "u": new Note("Bb4"),
+        "j": new Note("B4"),
+        "k": new Note("C5")
     }
-});
-document.addEventListener('keyup', function (e) {
-    if(notes[e.key]!=undefined){
-        document.getElementById(e.key).classList.remove("selected");
-        notes[e.key].stop();
-    }
+    document.addEventListener('keydown', function (e) {
+        if(notes[e.key]!=undefined){
+            document.getElementById(e.key).classList.add("selected");
+            notes[e.key].play()
+        }
+    });
+    document.addEventListener('keyup', function (e) {
+        if(notes[e.key]!=undefined){
+            document.getElementById(e.key).classList.remove("selected");
+            notes[e.key].stop();
+        }
+    });
 });
