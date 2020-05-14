616ae4d2ffe6cc18510d717ca6a3d2fdf4b49fe0
George Shanti <georgeshanti@gmail.com>
2020-05-14 16:52:22 +0530

    Visualizer

--- player.js
+++ player.js
@@ -22,6 +22,24 @@ class Note{
 //play a middle 'C' for the duration of an 8th note
 
 document.querySelector('button').addEventListener('click', async () => {
+    var colors = {
+        "1": 0,
+        "2": 0,
+        "3": 0,
+        "4": 0,
+    }
+
+    function colorize(){
+        var ov1 = document.getElementById("overlay-1");
+        var ov2 = document.getElementById("overlay-2");
+        var ov3 = document.getElementById("overlay-3");
+        var ov4 = document.getElementById("overlay-4");
+        ov1.style.background = "hsl("+((colors["1"]+1)*5*(360/128))%360+", 100%, 50%)";
+        ov2.style.background = "hsl("+((colors["4"]+1)*5*(360/128))%360+", 100%, 50%)";
+        ov3.style.background = "hsl("+((colors["2"]+1)*5*(360/128))%360+", 100%, 50%)";
+        ov4.style.background = "hsl("+((colors["3"]+1)*5*(360/128))%360+", 100%, 50%)";
+    }
+    colorize();
 	await Tone.start();
     console.log('audio is ready');
     document.getElementById("disclaimer").style.display="none";
@@ -30,32 +48,26 @@ document.querySelector('button').addEventListener('click', async () => {
     for( key of keys ){
         notes[key.getAttribute("id")] = new Note(key.getAttribute("note"));
     }
-    // var notes = {
-    //     "a": new Note("C4"),
-    //     "w": new Note("Db4"),
-    //     "s": new Note("D4"),
-    //     "e": new Note("Eb4"),
-    //     "d": new Note("E4"),
-    //     "f": new Note("F4"),
-    //     "t": new Note("Gb4"),
-    //     "g": new Note("G4"),
-    //     "y": new Note("Ab4"),
-    //     "h": new Note("A4"),
-    //     "u": new Note("Bb4"),
-    //     "j": new Note("B4"),
-    //     "k": new Note("C5")
-    // }
+    selected = [];
     document.addEventListener('keydown', function (e) {
         e.preventDefault();
-        if(notes[e.key]!=undefined){
-            document.getElementById(e.key).classList.add("selected");
+        if(notes[e.key]!=undefined && selected.indexOf(e.key)==-1){
+            selected.push(e.key);
+            var key = document.getElementById(e.key)
+            key.classList.add("selected");
+            colors[key.getAttribute("sec")] += parseInt(key.getAttribute("c"));
+            colorize();
             notes[e.key].play()
         }
     });
     document.addEventListener('keyup', function (e) {
         e.preventDefault();
-        if(notes[e.key]!=undefined){
-            document.getElementById(e.key).classList.remove("selected");
+        if(notes[e.key]!=undefined && selected.indexOf(e.key)!=-1){
+            selected.splice(selected.indexOf(e.key), 1);
+            var key = document.getElementById(e.key)
+            key.classList.remove("selected");
+            colors[key.getAttribute("sec")] -= parseInt(key.getAttribute("c"));
+            colorize();
             notes[e.key].stop();
         }
     });
